dafd2eab0eff041e92f91bfa4b9e450d4f7dddbd
fangyh1994 <31147954+fangyh1994@users.noreply.github.com>
2018-11-03 00:35:48 -0400

    hbs dynamic rendering

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Yehe Fang',
-        likes: [
-            'Biking',
-            'Cities'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page'); 
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +28,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
